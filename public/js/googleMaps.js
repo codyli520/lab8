2ffeafc221f4ff095511c4d899fbb34bd69a12f1
Longfei Li <codyli520@gmail.com>
2016-02-25 19:40:57 +0000

    thirdpartyAPI lab

---
+++ public/js/googleMaps.js
@@ -0,0 +1,17 @@
+function initMap() {
+  // Create center marker at UCSD
+  var ucsd_ltlng = {lat:32.885052, lng:-117.241269};
+    
+
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: ucsd_ltlng,
+    zoom: 18
+  });
+
+  var marker = new google.maps.Marker({
+      position: ucsd_ltlng,
+      map: map,
+      title: 'UCSD'
+  });
+}
